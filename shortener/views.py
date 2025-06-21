59bf78ecb8f7f4b8f89148dd918607b5eca59a37
Jakub <01187205@pw.edu.pl>
2025-06-21 16:25:18 +0200

    feat(Existing Shortcut): Added feature which prevents creating new shortcut for URL, if shortcut for this URL already exists in our database

--- shortener/views.py
+++ shortener/views.py
@@ -15,12 +15,22 @@ class CreateShortURL(APIView):
     def post(self, request):
         serializer = ShortURLSerializer(data=request.data)
         if serializer.is_valid():
+            original_url = serializer.validated_data['original_url']
+
+            # check if shortcut already exists, if yes then do not create new
+            existing = ShortURL.objects.filter(original_url=original_url).first()
+            if existing:
+                return Response({
+                    "short_url": f"http://localhost:8000/shrt/{existing.short_code}/"
+                }, status=status.HTTP_200_OK)
+            
+            
             code = generate_code()
             while ShortURL.objects.filter(short_code = code).exists():
                 code = generate_code()
             
             shorturl = ShortURL.objects.create(
-                original_url=serializer.validated_data['original_url'],
+                original_url=original_url,
                 short_code=code)
             
             return Response(
